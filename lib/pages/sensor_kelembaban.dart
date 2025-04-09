2feb68e0b6194c95ec271b4b92a1de1bfc718237
fadhilahnafth <fadhilahnaf6@gmail.com>
2025-04-09 12:47:53 +0700

    feat: add kelembapan

--- lib/pages/sensor_kelembaban.dart
+++ lib/pages/sensor_kelembaban.dart
@@ -1,1 +1,11 @@
+import 'package:flutter/material.dart';
 
+class SensorKelembapanPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Detail Sensor kalium")),
+      body: Center(child: Text("Halaman detail kalium")),
+    );
+  }
+}
